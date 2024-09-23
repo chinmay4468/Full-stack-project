81105f3c9b5c3a11ee40f574585ff3ed451d0a18
unknown <pchinmay546@gmail.com>
2024-09-22 21:28:22 -0400

    Added Register controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,9 +1,44 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "..utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { ApiResponse } from "../utils/apiResponse.js"
+
 
 const registerUser = asyncHandler( async(req,res)=> {
-    res.status(200).json({
-        message:"Chai aur code"
+    const {fullName, email, username, password} = req.body
+    if(
+        [fullName,email,username,password].some((field)=>field?.trim()==="")
+    ){
+        throw ApiError(400, "All fields are required")
+    }
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if(!regex.test(email))
+    {
+        throw ApiError(400, "Invalid Email format")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{username},{email}]  //check for all element in the array
     })
+    if(existedUser){
+        throw new ApiError(409, "User already exist")
+    }
+    const user = await User.create({
+        email,
+        fullname,
+        password,
+        username:username.toLowerCase()
+    })
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registering a user")
+    }
+    return res.status(201).json(
+        new ApiResponse(200, createdUser,"User Created Successfully")
+    )
+
 })
 
 export {registerUser}
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -27,7 +27,6 @@ const userSchema = new Schema(
         },
         avatar : {
             type: String,
-            required: true,
         },
         coverImage: {
             type: String
@@ -53,9 +52,38 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next
-        this.password = bcrypt.hash(this.password,10)
+        this.password = await bcrypt.hash(this.password,10)
         next()
 
 })
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
 
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 export const User =  mongoose.model("User", userSchema)
